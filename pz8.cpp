cef04cd1500d38b61e7d61a7afd25f9d43ad7a80
K1joL <100405888+K1joL@users.noreply.github.com>
2022-04-20 20:55:21 +0300

    Update pz8.cpp

--- pz8.cpp
+++ pz8.cpp
@@ -1,7 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 #define PIN 6
 #define count_led 64
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(count_led, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(count_led, PIN, NEO_GRB + NEO_KHZ800);colors
 void setup() {
   Serial.begin(9600);
   pixels.begin();
@@ -9,29 +9,38 @@ void setup() {
 
 void loop() {
   for (int k=0;k<count_led;k++)
- 	 pixels.setPixelColor(k,pixels.Color(0,0,0));
+  	pixels.setPixelColor(k,pixels.Color(0,0,0));
   pixels.show();
   delay(1000);
   
-  int arr[64]/*={
-  0,0,0,0,0,0,0,0,
-  0,1,0,0,0,0,1,0,
-  0,1,1,0,0,1,1,0,
-  0,0,0,0,0,0,0,0,
-  0,0,0,0,0,0,0,0,
-  0,0,1,0,0,1,0,0,
-  0,1,0,1,1,0,1,0,
-  0,0,0,0,0,0,0,0
-};*/;
-  for (int k = 0; k<64; k++)
-   arr[k] = rand()%2;
-    for (int i=0;i<64;i++){
-    		if (arr[i])
-              pixels.setPixelColor(i,pixels.Color(255,0,0));
+  int arr[8][8] = {
+  {0,1,1,0,0,1,1,0},
+  {1,1,1,1,1,1,1,1},
+  {1,1,1,1,1,1,1,1},
+  {1,1,1,1,1,1,1,1},
+  {1,1,1,1,1,1,1,1},
+  {0,1,1,1,1,1,1,0},
+  {0,0,1,1,1,1,0,0},
+  {0,0,0,1,1,0,0,0}
+};
+  int temp = 0;
+  for (int i=0; i<8;i++){
+    if (!(i%2)) continue;
+    for (int j=0; j<4;j++){
+      temp = arr[i][7-j];
+      arr[i][7-j] = arr[i][j];
+      arr[i][j] = temp;
     }
-    
-  pixels.show();
-  delay(1000);
-  
-  
+  }
+  int k = 0;
+  while (k < count_led){
+ 	  for (int i=0;i<8;i++){
+      for (int j=0; j<8; j++){
+    	if (arr[i][j]) pixels.setPixelColor(k,pixels.Color(255,0,0));
+     	k++;
+      }
+	  }
+  }
+     pixels.show();
+ 	 delay(1000);
 }
